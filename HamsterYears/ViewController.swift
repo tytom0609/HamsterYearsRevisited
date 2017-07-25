703d75a4a796b7621ae19e66638739d77e1a004e
Ty Tom <tytom@Tys-MacBook-Pro.local>
2017-07-25 11:05:35 -0500

    Initial Commit

---
+++ HamsterYears/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamsterYears
+//
+//  Created by Ty Tom on 7/25/17.
+//  Copyright © 2017 TyTom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
